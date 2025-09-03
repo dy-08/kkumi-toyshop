79d24b6a52fc0365746e9c6b8ced4c9eccaf6596
choieunjo <rei1626@naver.com>
2025-09-03 20:37:10 +0900

    fix: 헤더 요구사항 수정완료

--- src/js/header.js
+++ src/js/header.js
@@ -1,43 +1,36 @@
 export function renderHeader() {
-    const header = document.createElement('header');
-
+    const header = document.querySelector('header');
     header.innerHTML = `
-      <nav>
-        <div class="header__navBox">
-          <div class="header__leftBox">
-            <button><img src="./src/assets/images/header/menu.svg" alt="logo" />
-            <button>MUSINSA</button>
-            <button>BEAUTY</button>
-            <button>PLAYER</button>
-            <button>OUTLET</button>
-            <button>BOUTIQOE</button>
-            <button>SHOES</button>
-            <button>KIDS</button>
-            <button>(S)SNAP</button>
-          </div>
-          <div class="header__rightBox">
-            <button>오프라인스토어</button>
-            <button>
-              <span class="material-symbols-outlined">search</span
-              ><span>검색</span>
-            </button>
-            <button>
-              <span class="material-symbols-outlined">favorite</span
-              ><span>좋아요</span>
-            </button>
-            <button>
-              <span class="material-symbols-outlined">person</span
-              ><span>마이</span>
-            </button>
-            <button>
-              <span class="material-symbols-outlined">shopping_bag</span
-              ><span>장바구니</span>
-            </button>
-            <button>로그인 / 회원가입</button>
-          </div>
+    <nav>
+      <div class="header__navBox">
+        <div class="header__leftBox">
+          <span class="material-symbols-outlined menu">menu</span>
+          <button>MUSINSA</button>
+          <button>BEAUTY</button>
+          <button>PLAYER</button>
+          <button>OUTLET</button>
+          <button>BOUTIQOE</button>
+          <button>SHOES</button>
+          <button>KIDS</button>
+          <button>(S)SNAP</button>
         </div>
-      </nav>
+        <div class="header__rightBox">
+          <button>오프라인스토어</button>
+          <button>
+            <span class="material-symbols-outlined">search</span><span>검색</span>
+          </button>
+          <button>
+           <span class="material-symbols-outlined">favorite</span><span>좋아요</span>
+          </button>
+          <button>
+           <span class="material-symbols-outlined">person</span><span>마이</span>
+          </button>
+          <button>
+           <span class="material-symbols-outlined">shopping_bag</span><span>장바구니</span>
+          </button>
+          <button>로그인 / 회원가입</button>
+        </div>
+      </div>
+    </nav>
 `;
-    const app = document.getElementById('app');
-    app.appendChild(header);
 }
