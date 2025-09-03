a95cc23ffe946eea229c021d4c3eb71a86dccb50
choieunjo <rei1626@naver.com>
2025-09-03 17:51:36 +0900

    fix: 헤더 마크업 요청사항 수정완료

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,11 @@
 export function renderHeader() {
-    const section = document.createElement('section');
-    section.id = 'header';
+    const header = document.createElement('header');
 
-    section.innerHTML = `
+    header.innerHTML = `
       <nav>
         <div class="header__navBox">
           <div class="header__leftBox">
-            <span class="material-symbols-outlined menu">menu</span>
+            <button><img src="./src/assets/images/header/menu.svg" alt="logo" />
             <button>MUSINSA</button>
             <button>BEAUTY</button>
             <button>PLAYER</button>
@@ -19,16 +18,20 @@ export function renderHeader() {
           <div class="header__rightBox">
             <button>오프라인스토어</button>
             <button>
-              <span class="material-symbols-outlined">search</span><span>검색</span>
+              <span class="material-symbols-outlined">search</span
+              ><span>검색</span>
             </button>
             <button>
-            <span class="material-symbols-outlined">favorite</span><span>좋아요</span>
+              <span class="material-symbols-outlined">favorite</span
+              ><span>좋아요</span>
             </button>
             <button>
-            <span class="material-symbols-outlined">person</span><span>마이</span>
+              <span class="material-symbols-outlined">person</span
+              ><span>마이</span>
             </button>
             <button>
-            <span class="material-symbols-outlined">shopping_bag</span><span>장바구니</span>
+              <span class="material-symbols-outlined">shopping_bag</span
+              ><span>장바구니</span>
             </button>
             <button>로그인 / 회원가입</button>
           </div>
@@ -36,5 +39,5 @@ export function renderHeader() {
       </nav>
 `;
     const app = document.getElementById('app');
-    app.appendChild(section);
+    app.appendChild(header);
 }
