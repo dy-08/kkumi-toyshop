889fd7c7bf9091fdbc4a189c6fab47de358f6c1e
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-09-06 00:32:31 +0900

    fix: 헤더 머지 후 렌더링 오류 → 컴포넌트 재작성으로 해결

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,12 @@
 export function renderHeader() {
     const section = document.createElement('section');
     section.id = 'header';
-
     section.innerHTML = `
       <nav>
         <div class="header__navBox">
           <div class="header__leftBox">
-            <span class="material-symbols-outlined menu">menu</span>
+            <button class="header__menu"></button>
+            <span class="header__vertical"></span>
             <button>MUSINSA</button>
             <button>BEAUTY</button>
             <button>PLAYER</button>
@@ -14,23 +14,26 @@ export function renderHeader() {
             <button>BOUTIQOE</button>
             <button>SHOES</button>
             <button>KIDS</button>
-            <button>(S)SNAP</button>
+            <button>UESD</button>
+            <span class='header__vertical'></span>
+            <button class="header__snap"><span class="header__icons icon__chars"></span>SNAP</button>
           </div>
           <div class="header__rightBox">
             <button>오프라인스토어</button>
+            <span class='header__vertical'></span>
             <button>
-              <span class="material-symbols-outlined">search</span><span>검색</span>
+              <span class="header__icons icon__search"></span><span>검색</span>
             </button>
             <button>
-            <span class="material-symbols-outlined">favorite</span><span>좋아요</span>
+            <span class="header__icons icon__like"></span><span>좋아요</span>
             </button>
             <button>
-            <span class="material-symbols-outlined">person</span><span>마이</span>
+            <span class="header__icons icon__mypage"></span><span>마이</span>
             </button>
             <button>
-            <span class="material-symbols-outlined">shopping_bag</span><span>장바구니</span>
+            <span class="header__icons icon__cart"></span><span>장바구니</span>
             </button>
-            <button>로그인 / 회원가입</button>
+            <button class="signinAndUp">로그인 / 회원가입</button>
           </div>
         </div>
       </nav>
--- src/js/search.js
+++ src/js/search.js
@@ -199,48 +199,53 @@ export function renderSearch() {
         document.querySelector('.keyword__box').remove();
     });
 
-    
     setInterval(() => {
-      // 날짜계산
-      let today = new Date();
-      let month = today.getMonth() + 1;
-      let date = today.getDate();
-      let todayWrap = document.querySelectorAll('.search__keyword__time');
-  
-      //시간계산
-      let hours = today.getHours();
+        // 날짜계산
+        let today = new Date();
+        let month = today.getMonth() + 1;
+        let date = today.getDate();
+        let todayWrap = document.querySelectorAll('.search__keyword__time');
 
-      todayWrap.forEach((time) => {
-          if (month < 10) {
-            if(date < 10){
-              time.innerText = `0${month}.0${date} ${hours}:00, 기준`;
-            }else{
-              time.innerText = `0${month}.${date} ${hours}:00, 기준`;
-            }
-          } else {
-            if(date < 10){
-              time.innerText = `${month}.0${date} ${hours}:00, 기준`;
-            }else{
-              time.innerText = `${month}.${date} ${hours}:00, 기준`;
-            }
-          }
-      }, 1000 * 60 * 60);
+        //시간계산
+        let hours = today.getHours();
+
+        todayWrap.forEach(
+            (time) => {
+                if (month < 10) {
+                    if (date < 10) {
+                        time.innerText = `0${month}.0${date} ${hours}:00, 기준`;
+                    } else {
+                        time.innerText = `0${month}.${date} ${hours}:00, 기준`;
+                    }
+                } else {
+                    if (date < 10) {
+                        time.innerText = `${month}.0${date} ${hours}:00, 기준`;
+                    } else {
+                        time.innerText = `${month}.${date} ${hours}:00, 기준`;
+                    }
+                }
+            },
+            1000 * 60 * 60
+        );
     });
-
-
 
     // 메인탭 상단 고정
     let mainTab = document.querySelector('.mainTab');
     let bodyF = document.querySelector('body');
-    let mainTabOffsetTop = mainTab.offsetTop - 58;
+    // Last updated: 25-09-05 by권영호, 스크롤이벤트 조건 타겟 변경 ( mainTab.offsetTop - 58 -> .search__wrap.offsetHeight)
+    let searchWrapHeight = document.querySelector('.search__wrap').offsetHeight; // 113px
+    // let mainTabOffsetTop = mainTab.offsetTop - 58; 111px
+
     function scrollTab() {
         let windowS = window.scrollY;
-        if (windowS >= mainTabOffsetTop) {
+        if (windowS >= searchWrapHeight) {
             mainTab.classList.add('fixed');
-            bodyF.style.paddingTop = '108px';
+            // Last updated: 25-09-05 by권영호, 패딩높이수정 (108px->104px)
+            bodyF.style.paddingTop = '104px';
         } else {
             mainTab.classList.remove('fixed');
-            bodyF.style.paddingTop = '60px';
+            // Last updated: 25-09-05 by권영호, 패딩높이수정 (60px->56px)
+            bodyF.style.paddingTop = '56px';
         }
     }
     window.addEventListener('scroll', scrollTab);
