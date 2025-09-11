ad4f8c76acab5925d88c9a296397a3342720c88c
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-09-11 22:36:45 +0900

    브렌치변경 (feat/detail-product-select-ui -> 23-featdetail-product-select-ui)

--- src/js/detail.js
+++ src/js/detail.js
@@ -59,8 +59,86 @@ export function renderDetail() {
                 </div>
             </div>
             <div class="detail__right__wrap">
-                <div class="detail__payment__info">
-                    내용
+                <div class="detail__selectBox">
+                    <div class="detail__selectInnerBox">
+                        <div class="detail__productInfo">
+                            <div class="detail__productBrandInfo">
+                                <div>
+                                    <div class="detail__icons detail__brandLogo">
+                                        <img alt"브랜드 로고이미지"></img>
+                                    </div>
+                                    <div class="detail__brandName"></div>
+                                    <!-- json에 badge data 추가해야 됨 (badge) ✨-->
+                                    <div class="badge"></div>
+                                </div>
+                                <div>
+                                    <button>
+                                        <span class="detail__icons detail__iconBrandFavorite"></span>
+                                        <span class="detail__brandFavoriteTotal">17만</span>
+                                    </button> 
+                                </div>
+                            </div>
+                            <div class="detail__productLocation">
+                                <span>아우터 &gt; <span class="detail__productCategory"></span></span>
+                            </div>
+                            <div class="detail__productName"></div>
+                            <div class="badges"></div>
+                            <!-- json에 badge data 추가해야 됨 (colors) ✨-->
+                            <div class="detail__colorsdisplay">
+                            </div>
+                            <div>
+                                <span class="detail__originPrice"></span>
+                            </div>
+                            <div>
+                                <span class="detail__discount"></span><span class="detail__discountPrice"></span>
+                            </div>
+                        </div>
+                        <div class="detail__productForm">
+                            <ul class="detail__productForm__colors"><span id='detail__colors__firstSpan'></span><span class="detail__productForm__colorsTitle">컬러</span></ul>
+                            <ul class="detail__productForm__sizes">사이즈</ul>
+                            <div class="detail__product__display--selected">
+                                <div class="detail__product__displayInner">
+                                    <div class="u-text-14 detail__seletedProduct__display">
+                                        colorsdisplay+colorEG추가+size
+                                    </div>
+                                    <div class="detail__icons detail__iconClose"></div>
+                                </div>
+                                <div class="detail__product__displayInner">
+                                    <div>
+                                        <div class="detail__product__quantityWrap">
+                                            <button class="detail__icons detail__iconMinus" disabled></button>
+                                            <input type="number" class="detail__product__quantity u-text-13" value="1" disabled/>
+                                            <button class="detail__icons detail__iconPlus"></button>
+                                        </div>
+                                        <div>
+                                            <span class="u-text-11 u-color-8a">최대 3개</span>
+                                        </div>
+                                    </div>
+                                    <span class="detail__discountPrice u-text-14"></span>
+                                </div>
+                            </div>
+                            <div class="detail__btns">
+                                <div>
+                                    <button>
+                                        <div class="detail__icons detail__iconFavorite"></div>
+                                        <div>3천</div>
+                                    </button>
+                                </div>
+                                <button>장바구니</button>
+                                <button>구매하기</button>
+                            </div>
+                        </div>
+                        <div class="detail__productDeliveryInfo">
+                            <div>
+                                무신사 회원은 전 품목 무료배송<br>
+                                <span>&#40;일부 상품 및 도서 산간 지역 제외&#41;</span>
+                            </div>
+                            <div>
+                                09&#46;09 &#40;화&#41; 도착 예정 · 도착 확률 99&#37;<span class="detail__icons detail__iconInformation"></span><br>
+                                결제 3일 이내 출고 · CJ대한통운
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -85,23 +163,23 @@ export function renderDetail() {
     let smallItem = smallImgBox.querySelectorAll('li');
     smallItem[0].classList.add('on');
 
-    function itemMatch(){
+    function itemMatch() {
         smallItem.forEach((i) => {
-            i.classList.remove('on')
+            i.classList.remove('on');
         });
-        smallItem[index-1].classList.add('on');
+        smallItem[index - 1].classList.add('on');
     }
     smallItem.forEach((item, idx) => {
         item.addEventListener('click', () => {
             smallItem.forEach((i) => {
-            i.classList.remove('on');
-        });
+                i.classList.remove('on');
+            });
             item.classList.add('on');
-            slideBox.style.transform = `translateX(-${step * (idx+1)}px)`;
-            index = idx+1;
+            slideBox.style.transform = `translateX(-${step * (idx + 1)}px)`;
+            index = idx + 1;
             updateCount(index);
         });
-    })
+    });
 
     let slideItem = document.querySelectorAll('.detail__slide__box li'); // 슬라이드 li
     let slideCount = document.querySelector('.detail__slide__count');
@@ -122,7 +200,6 @@ export function renderDetail() {
     slideBox.style.transition = 'transform 0.3s ease';
     slideBox.style.transform = `translateX(-${step * index}px)`;
 
-    
     function updateCount() {
         let showIndex = index;
         if (index == 0) showIndex = imgTotal - 2;
@@ -149,11 +226,11 @@ export function renderDetail() {
 
     window.nextBtn = function () {
         next();
-    }
+    };
 
     window.prevBtn = function () {
         prev();
-    }
+    };
 
     slideBox.addEventListener('transitionend', () => {
         // 마지막 복제 슬라이드
@@ -181,8 +258,96 @@ export function renderDetail() {
 
     // 슬라이드 크게보기 버튼
     let scaleBtn = document.getElementById('scaleBtn');
-    scaleBtn.addEventListener('click', () =>{
+    scaleBtn.addEventListener('click', () => {
         document.querySelector('.detail__img__slide').classList.add('scale');
         document.querySelector('body').classList.add('scale');
+    });
+
+    // 로컬스토리지에서 아이템데이터 받아와서 할당
+    const data = localStorage.getItem('item');
+    const item = JSON.parse(data);
+    const brandLogo = document.querySelector('.detail__brandLogo img');
+    brandLogo.src = item.logoSrc;
+    const brandName = document.querySelector('.detail__brandName');
+    brandName.textContent = item.brandName;
+    const category = document.querySelector('.detail__productCategory');
+    category.textContent = `${item.category} (${item.brandName})`;
+    const productName = document.querySelector('.detail__productName');
+    productName.textContent = item.itemName;
+    const colorsdisplay = document.querySelector('.detail__colorsdisplay');
+    item.colors.forEach((item) => {
+        const span = document.createElement('span');
+        span.style.backgroundColor = item;
+        colorsdisplay.appendChild(span);
+    });
+    const originPrice = document.querySelector('.detail__originPrice');
+    originPrice.textContent = item.originPrice;
+    const discountRate = document.querySelector('.detail__discount');
+    discountRate.textContent = `${item.discount}%`;
+    const discountPrice = document.querySelectorAll('.detail__discountPrice');
+    discountPrice.forEach((content) => {
+        content.textContent = `${item.discountPrice}원`;
+    });
+
+    const formColor = document.querySelector('.detail__productForm__colors');
+    const formSize = document.querySelector('.detail__productForm__sizes');
+    const oldSpan = document.querySelector('.detail__productForm__colorsTitle');
+
+    // 셀렉폼 렌더링
+    // 컬러
+    formColor.addEventListener('click', () => {
+        formColor.innerHTML = '';
+        formColor.classList.toggle('selected');
+        formColor.classList.toggle('radius');
+        formSize.classList.toggle('removed');
+
+        oldSpan.className = 'detail__productForm__colorsTitle';
+        oldSpan.textContent = '컬러';
+        formColor.appendChild(oldSpan);
+
+        // li 추가
+        item.colors.forEach((color, idx) => {
+            const span = document.createElement('span');
+            span.className = 'detail__productColor';
+            span.style.backgroundColor = color;
+            const li = document.createElement('li');
+            li.textContent = item.colorsKR[idx];
+            li.appendChild(span);
+            formColor.appendChild(li);
+        });
+        // 컬러
+        const colors = formColor.querySelectorAll('li');
+        colors.forEach((color, index) =>
+            color.addEventListener('click', () => {
+                console.log(index);
+                console.log('2:', item.colors[index]);
+                console.log(item.colorsEG[index].toUpperCase());
+                console.log(formColor.firstChild);
+                console.log(oldSpan);
+
+                const span = document.createElement('span');
+                span.className = 'detail__productForm__colorsTitle';
+                span.style.paddingLeft = '26px';
+                span.style.transform = 'translateY(-1px)';
+                span.textContent = item.colorsEG[index].toUpperCase();
+                formColor.replaceChild(span, oldSpan);
+
+                // formColor.textContent = item.colorsEG[index].toUpperCase();
+                // console.log(colorsTitle);
+                // firstSpan.style.backgroundColor = `${item.colors[index]}`;
+            })
+        );
+    });
+    // 사이즈
+    formSize.addEventListener('click', () => {
+        formSize.classList.toggle('radius');
+        formSize.classList.toggle('selected');
+        formSize.innerHTML = '';
+        formSize.textContent = '사이즈';
+        item.size.forEach((size) => {
+            const li = document.createElement('li');
+            li.textContent = size;
+            formSize.appendChild(li);
+        });
     });
 }
